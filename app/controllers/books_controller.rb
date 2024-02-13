327ca880ecd3bb98a3b17956e2a26ce97d3b632a
EC2 Default User <ec2-user@ip-172-31-7-37.ap-northeast-1.compute.internal>
2024-02-13 11:23:21 +0000

    [Add]css適用

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,17 +1,20 @@
 class BooksController < ApplicationController
-  def new
+
+  def index
+    @books = Book.all
     @book = Book.new
   end
   
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end 
   end 
-  
-  def index
-    @books = Book.all
-  end
 
   def show
     @book = Book.find(params[:id])
@@ -27,6 +30,12 @@ class BooksController < ApplicationController
     redirect_to book_path(book.id)
   end 
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end 
+
   private
   # ストロングパラメーター
   def book_params
